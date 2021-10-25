ac638c366315750076a2e9f18c66d8113a325ec7
romauniat <92386260+romauniat@users.noreply.github.com>
2021-10-25 13:35:57 -0400

    Create A2CodingBatLogic12.java
    add code starter for A2. Optional submit file.

---
+++ A2CodingBatLogic12.java
@@ -0,0 +1,37 @@
+/*
+Name:
+Date:
+Description: Add my own test cases some of the problems 
+from codingbat Logic1 and Logic2.
+*/
+
+class A2CodingBatLogic12 {
+
+  //copy and paste the method from coding bat here.
+
+
+
+
+  //This method returns true if the party was a success.
+  public static boolean cigarParty(int cigars, boolean isWeekend){
+    return true;
+  }// close cigarParty
+
+
+
+
+
+  public static void main(String[] args) {
+  //call the method(s) inside the main with the test case values.
+
+
+
+
+  // call the cigarParty method and then print the result.
+  boolean isSuccess = cigarParty(30,false);
+  System.out.println("cigarParty: "+ isSuccess);
+
+
+
+  }// close main
+}
